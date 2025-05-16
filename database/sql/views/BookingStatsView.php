860b880b16f976f8169b663997d6db5fd4901ed0
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-05-16 23:53:10 +0800

    Added BookingStatsView

---
+++ database/sql/views/BookingStatsView.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+return function () {
+
+
+    DB::statement("DROP VIEW IF EXISTS booking_stats_view");
+
+    DB::statement("
+            CREATE VIEW booking_stats_view AS
+    SELECT 
+        'Pending' AS status,
+        SUM(CASE WHEN status = 'Pending' AND check_in = CURDATE() THEN 1 ELSE 0 END) AS today,
+        SUM(CASE WHEN status = 'Pending' AND check_in >= CURDATE() - INTERVAL 6 DAY THEN 1 ELSE 0 END) AS this_week,
+        SUM(CASE WHEN status = 'Pending' AND check_in >= CURDATE() - INTERVAL 29 DAY THEN 1 ELSE 0 END) AS this_month,
+        SUM(CASE WHEN status = 'Pending' AND check_in >= CURDATE() - INTERVAL 364 DAY THEN 1 ELSE 0 END) AS this_year
+    FROM bookings
+
+    UNION ALL
+
+    SELECT 
+        'Confirmed',
+        SUM(CASE WHEN status = 'Confirmed' AND check_in = CURDATE() THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Confirmed' AND check_in >= CURDATE() - INTERVAL 6 DAY THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Confirmed' AND check_in >= CURDATE() - INTERVAL 29 DAY THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Confirmed' AND check_in >= CURDATE() - INTERVAL 364 DAY THEN 1 ELSE 0 END)
+    FROM bookings
+
+    UNION ALL
+
+    SELECT 
+        'Cancelled',
+        SUM(CASE WHEN status = 'Cancelled' AND check_in = CURDATE() THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Cancelled' AND check_in >= CURDATE() - INTERVAL 6 DAY THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Cancelled' AND check_in >= CURDATE() - INTERVAL 29 DAY THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Cancelled' AND check_in >= CURDATE() - INTERVAL 364 DAY THEN 1 ELSE 0 END)
+    FROM bookings
+
+    UNION ALL
+
+    SELECT 
+        'Completed',
+        SUM(CASE WHEN status = 'Completed' AND check_out = CURDATE() THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Completed' AND check_out >= CURDATE() - INTERVAL 6 DAY THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Completed' AND check_out >= CURDATE() - INTERVAL 29 DAY THEN 1 ELSE 0 END),
+        SUM(CASE WHEN status = 'Completed' AND check_out >= CURDATE() - INTERVAL 364 DAY THEN 1 ELSE 0 END)
+    FROM bookings;
+    ");
+};
